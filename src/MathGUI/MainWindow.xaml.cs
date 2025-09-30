82091e0390d2b8bd23b73ab337119350dde35911
Charlie Flux <mail@charlieflux.com>
2025-09-30 09:43:27 +0100

    Added plotting library and intial GUI design

---
+++ src/MathGUI/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using OxyPlot;
+using OxyPlot.Series;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MathGUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        // Status TextBlock
+        private string statusMessage = string.Empty;
+
+        public MainViewModel()
+        {
+            this.MyModel = new PlotModel { Title = "Example 1" };
+            this.MyModel.Series.Add(new FunctionSeries(Math.Cos, 0, 10, 0.1, "cos(x)"));
+
+            StatusMessage = "Welcome";
+
+        }
+
+        public PlotModel MyModel { get; private set; }
+
+        public string StatusMessage
+        {
+            get => statusMessage;
+            set
+            {
+                if(statusMessage != value)
+                {
+                    statusMessage = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+}
