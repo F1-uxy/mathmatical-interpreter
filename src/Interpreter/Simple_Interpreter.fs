b249484fc8bddeef07e432c3eb3306d53bc7790d
Charlie Flux <mail@charlieflux.com>
2025-10-08 09:08:25 +0100

    Implemented modulo operator

--- src/Interpreter/Simple_Interpreter.fs
+++ src/Interpreter/Simple_Interpreter.fs
@@ -8,7 +8,7 @@ module interpreter
 open System
 
 type terminal = 
-    Add | Sub | Mul | Div | Lpar | Rpar | Num of int
+    Add | Sub | Mul | Div | Mod | Lpar | Rpar | Num of int
 
 let str2lst s = [for c in s -> c]
 let isblank c = System.Char.IsWhiteSpace c
@@ -29,6 +29,7 @@ let lexer input =
         | '+'::tail -> Add :: scan tail
         | '-'::tail -> Sub :: scan tail
         | '*'::tail -> Mul :: scan tail
+        | '%'::tail -> Mod :: scan tail
         | '/'::tail -> Div :: scan tail
         | '('::tail -> Lpar:: scan tail
         | ')'::tail -> Rpar:: scan tail
@@ -46,7 +47,7 @@ let getInputString() : string =
 // <E>        ::= <T> <Eopt>
 // <Eopt>     ::= "+" <T> <Eopt> | "-" <T> <Eopt> | <empty>
 // <T>        ::= <NR> <Topt>
-// <Topt>     ::= "*" <NR> <Topt> | "/" <NR> <Topt> | <empty>
+// <Topt>     ::= "%" <NR> <Topt> | "*" <NR> <Topt> | "/" <NR> <Topt> | <empty>
 // <NR>       ::= "+" <NR> | "-" <NR> | "Num" <value> | "(" <E> ")"
 
 let parser tList = 
@@ -61,6 +62,7 @@ let parser tList =
         match tList with
         | Mul :: tail -> (NR >> Topt) tail
         | Div :: tail -> (NR >> Topt) tail
+        | Mod :: tail -> (NR >> Topt) tail
         | _ -> tList
     and NR tList =
         match tList with
@@ -89,6 +91,8 @@ let parseNeval tList =
                          Topt (tLst, value * tval)
         | Div :: tail -> let (tLst, tval) = NR tail
                          Topt (tLst, value / tval)
+        | Mod :: tail -> let (tLst, tval) = NR tail
+                         Topt (tLst, value % tval)
         | _ -> (tList, value)
     and NR tList =
         match tList with
